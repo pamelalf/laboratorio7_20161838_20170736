740abceaa04f903b2b3e1f4aba2ab5517f7c0e58
Pamela Luque <pamela.luque@pucp.edu.pe>
2021-06-08 22:34:31 -0500

    segundo

---
+++ js/resultado.js
@@ -0,0 +1,15 @@
+// Para generar la información personal
+let nombres = ["Jesús", "Alejandro", "Andrea", "Bruno", "Daniel", "Víctor", "Sofía", "Alonso", "Israel", "Mayra"]
+let apellidos = ["Gayoso", "Morterero", "Montes", "Barrios", "Sánchez", "Alcabú", "Barrios", "Sáez", "Obregón", "Franco"]
+
+// Locales de votación
+let locales = ["Pontificia Universidad Católica del Perú",
+  "Colegio San Agustín",
+  "Campo de Marte",
+  "Estadio Nacional",
+  "Residencial San Felipe",
+  "Estadio Daniel Alcides Carrión"
+]
+
+// Para generar la información si es miembro de mesa
+let cargos = ["Presidente", "Secretario", "Tercer Miembro", "Primer Suplente", "Segundo Suplente", "Tercer Suplente"]
